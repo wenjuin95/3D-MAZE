f42c9516dba98e67073ccc827cfc166f3aa09c6f
Wen Juin Low <welow@u91z04s05.42kl.edu.my>
2024-11-27 22:04:32 +0800

    feat: did until [xpm_to_img]

--- src/main.c
+++ src/main.c
@@ -6,18 +6,11 @@
 /*   By: welow <welow@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/18 19:57:52 by welow             #+#    #+#             */
-/*   Updated: 2024/11/27 21:05:14 by welow            ###   ########.fr       */
+/*   Updated: 2024/11/27 22:03:51 by welow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
-
-int	close_win(t_data *data)
-{
-	mlx_destroy_window(data->mlx, data->win);
-	free(data->mlx);
-	exit(0);
-}
 
 void	initialize_data(t_data *data)
 {
@@ -27,8 +20,49 @@ void	initialize_data(t_data *data)
 	data->texture.tex_size = TEXTURE_SIZE;
 }
 
-void	initialize_ime(t_data *data)
+int	*xpm_to_img(t_data *data, char *path)
 {
+	t_img	img;
+	int		*buffer;
+	int		x;
+	int		y;
+
+	init_texture(data, path, &img); //tomorrow continue
+	buffer = ft_calloc(1, sizeof * buffer
+			* data->texture.tex_size * data->texture.tex_size);
+	if (buffer == NULL)
+		clean_and_exit(data);
+	y = 0;
+	while (y < data->texture.tex_size)
+	{
+		x = 0;
+		while (x < data->texture.tex_size)
+		{
+			buffer[y * data->texture.tex_size + x]
+				= img.img_addr[y * data->texture.tex_size + x];
+			x++;
+		}
+		y++;
+	}
+	mlx_destroy_image(data->mlx, img.img);
+	return (buffer);
+}
+
+/**
+ * @brief initialize the texture for north, south, west, and east
+ * @param data the north, south, west, and east texture
+ * @return void
+ * @note 1. allocate 4 memory for the texture
+*/
+void	initialize_img(t_data *data)
+{
+	data->tex = ft_calloc(4, sizeof * data->tex);
+	if (data->tex == NULL)
+		clean_and_exit(data);
+	data->tex[NORTH] = xpm_to_img(data, data->texture.north);
+	data->tex[SOUTH] = xpm_to_img(data, data->texture.south);
+	data->tex[WEST] = xpm_to_img(data, data->texture.west);
+	data->tex[EAST] = xpm_to_img(data, data->texture.east);
 }
 
 int	main(int ac, char **av)
