9cfa1d5bc2b8b12e39a2bec9d0f1601d3f4b5987
Cheng Soo Tan <chtan@u90z06s02.42kl.edu.my>
2024-12-06 14:03:22 +0800

    feat add a function check wall is serounded by wall

--- src/convert_rgb.c
+++ src/convert_rgb.c
@@ -6,7 +6,7 @@
 /*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 11:09:46 by chtan             #+#    #+#             */
-/*   Updated: 2024/12/05 16:37:02 by chtan            ###   ########.fr       */
+/*   Updated: 2024/12/06 13:41:45 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,6 +39,8 @@ static int	*copy_to_rgb_array(char **rgb_to_convert, int *rgb)
 	}
 	i = 0;
 	free(rgb_to_convert);
+	if (check_valid_rgb(rgb) == 1)
+		return (0);
 	return (rgb);
 }
 
