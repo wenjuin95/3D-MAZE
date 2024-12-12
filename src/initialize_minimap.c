5175f18afeafa3c2151c709ecb817c85ebab8779
low <lowwenjuin27@gmail.com>
2024-12-12 21:57:37 +0800

    fix: norm error

--- src/initialize_minimap.c
+++ src/initialize_minimap.c
@@ -3,14 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   initialize_minimap.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: welow <welow@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: welow < welow@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/08 23:24:23 by welow             #+#    #+#             */
-/*   Updated: 2024/12/12 11:40:14 by welow            ###   ########.fr       */
+/*   Updated: 2024/12/12 21:56:35 by welow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
+
+/**
+ * @brief check each line of the minimap and give the value
+ * @param data get the player position, minimap visible x and y
+ * @param x x coordinate visible in the minimap
+ * @param y y coordinate visible in the minimap
+*/
+char	initialize_line(t_data *data, int x, int y)
+{
+	int	minimap_x;
+	int	minimap_y;
+
+	minimap_x = x + data->minimap.visible_x;
+	minimap_y = y + data->minimap.visible_y;
+	if ((int)data->player.pos_x == (minimap_x)
+		&& (int)data->player.pos_y == (minimap_y))
+		return ('P');
+	else if (data->map.map_layout[minimap_y][minimap_x] == '1')
+		return ('1');
+	else if (data->map.map_layout[minimap_y][minimap_x] == '0')
+		return ('0');
+	else if (data->map.map_layout[minimap_y][minimap_x] == ' ')
+		return (' ');
+	else
+		return ('\0');
+}
 
 /**
  * @brief search what to assign in each minimap line
@@ -18,7 +44,8 @@
  * 			   minimap offset x and y
  * @param y height of the minimap
  * @return the minimap line
- * @note 1. if the player position is same with x and y, assign 'P' to the line
+ * @note 1. if the player position is same with x and y, assign 'P'
+ * 			to the line
 */
 char	*add_minimap_line(t_data *data, int y)
 {
@@ -31,17 +58,7 @@ char	*add_minimap_line(t_data *data, int y)
 	x = 0;
 	while (x < data->minimap.size)
 	{
-		if ((int)data->player.pos_x == (x + data->minimap.visible_x)
-			&& (int)data->player.pos_y == (y + data->minimap.visible_y))
-			line[x] = 'P';
-		else if (data->map.map_layout[y + data->minimap.visible_y][x + data->minimap.visible_x] == '1')
-			line[x] = '1';
-		else if (data->map.map_layout[y + data->minimap.visible_y][x + data->minimap.visible_x] == '0')
-			line[x] = '0';
-		else if (data->map.map_layout[y + data->minimap.visible_y][x + data->minimap.visible_x] == ' ')
-			line[x] = ' ';
-		else
-			line[x] = '\0';
+		line[x] = initialize_line(data, x, y);
 		x++;
 	}
 	return (line);
@@ -52,8 +69,8 @@ char	*add_minimap_line(t_data *data, int y)
  * @param data get the minimap size and map height
  * @return the minimap map
  * @note 1. The function allocates memory for the minimap map.
- * @note 2. The function then iterates through the minimap size and map height to add each
- * 			line of the minimap to the map.
+ * @note 2. The function then iterates through the minimap size and map
+ * 			height to add each line of the minimap to the map.
 */
 char	**get_minimap(t_data *data)
 {
@@ -110,8 +127,8 @@ int	get_visible_map(t_data *data, int size, int pos)
  * @brief put minimap to the window
  * @param data get minimap_view_distance, minimap_size, minimap_texture_size,
  * 			   minimap_offset_x, minimap_offset_y and minimap_map
- * @note 1. get the length how far for player to up, down, left and right to the
- * 			end of the minimap
+ * @note 1. get the length how far for player to up, down, left and right to
+ * 			the end of the minimap
  * @note 2. get the size of the minimap
  * @note 3. get the image size for the minimap
  * @note 4. get the visible part of the map in player's position in x-axis
@@ -123,8 +140,10 @@ void	put_minimap(t_data *data)
 	data->minimap.view_distance = MAP_VIEW;
 	data->minimap.size = (2 * data->minimap.view_distance);
 	data->minimap.texture_size = MAP_PIXEL / data->minimap.size;
-	data->minimap.visible_x = get_visible_map(data, data->map.map_width, (int)data->player.pos_x);
-	data->minimap.visible_y = get_visible_map(data, data->map.maply_height, (int)data->player.pos_y);
+	data->minimap.visible_x = get_visible_map(data,
+		data->map.map_width, (int)data->player.pos_x);
+	data->minimap.visible_y = get_visible_map(data,
+		data->map.maply_height, (int)data->player.pos_y);
 	data->minimap.map = get_minimap(data);
 	if (data->minimap.map == NULL)
 		return ;
