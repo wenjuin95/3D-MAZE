32d895b197bdd10dd14af98f12d4dbdf9613fa56
Wen Juin Low <welow@u82z01s06.42kl.edu.my>
2024-12-30 18:58:31 +0800

    feat: remove unwanted

--- function_mlx_guide/initialize_mlx.c
+++ function_mlx_guide/initialize_mlx.c
@@ -16,7 +16,10 @@ void my_pixel_put(t_data *data, int x, int y, int color)
 {
 	char *dst;
 
+	//calculate the positoin of the pixel to the address of the image
 	dst = data->addr + (y * data->size_line + x * (data->bits_per_pixel / 8));
+
+	//set the color of the pixel to the address
 	*(unsigned int *)dst = color;
 }
 
@@ -26,11 +29,22 @@ int main()
 	void *win;
 	t_data img;
 
+	//mlx_init is the connection to the display server
 	mlx = mlx_init();
+
+	//create a window
 	win = mlx_new_window(mlx, WIDTH, HEIGHT, "mlx 42");
 
-	//initialize
+	//create an image with the size of the window
 	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+
+	//get the information aboute the image and allow for modification
+	//bit_per_pixel mean the number of bits used to represent a pixel color
+	// - 32 because a pixel is coded on 4 char, those chars worth 8 bits each, which gives us 32.
+	//size_line is the size of a line in bytes
+	// - 3200 because the width of one line of pixel is 800, times 4, because a pixel is coded on 4 chars
+	//endian is to tell you if the pixel color is stored in little endian or big endian
+	// - 0 or 1, depending of the architecture of the computer you use (more informations in the link above).
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.size_line, &img.endian);
 
 	//draw
@@ -40,6 +54,8 @@ int main()
 
 	//put image to window
 	mlx_put_image_to_window(mlx, win, img.img, 0, 0);
+
+	//loop to keep the window open
 	mlx_loop(mlx);
 	return (0);
 }
--- function_mlx_guide/mlx_mouse_move.c
+++ function_mlx_guide/mlx_mouse_move.c
@@ -32,9 +32,21 @@ int	close_win(void *data)
 	exit(0);
 }
 
+int	button(int keycode, t_data *data)
+{
+	(void)data;
+	if (keycode == 65307)
+		exit(0);
+	return (0);
+}
+
 int handle_mouse(int x, int y, t_data *data)
 {
+	//if mouse right near the edge of the screen, move it to the center
 	if (x >= WIDTH - 20)
+		mlx_mouse_move(data->mlx, data->win, WIDTH / 2, y);
+	//if mouse left near the edge of the screen, move it to the center
+	if (x <= 20)
 		mlx_mouse_move(data->mlx, data->win, WIDTH / 2, y);
 	return (0);
 }
@@ -49,16 +61,24 @@ int main()
 	//initialize
 	data.img = mlx_new_image(data.mlx, WIDTH, HEIGHT);
 	data.addr = mlx_get_data_addr(data.img, &data.bits_per_pixel, &data.size_line, &data.endian);
+	printf("bits_per_pixel: %d\n", data.bits_per_pixel);
+	printf("size_line: %d\n", data.size_line);
+	printf("endian: %d\n", data.endian);
 
 	//draw
 	for (int i = 0; i < WIDTH; i++)
 		for (int j = 0; j < HEIGHT; j++)
 			my_pixel_put(&data, i, j, 0x00FF00);
 
-	//for close window
-	mlx_hook(data.win, DestroyNotify, NoEventMask, close_win, NULL);
-
-	mlx_hook(data.win, MotionNotify, PointerMotionMask, handle_mouse, &data);
+	//mlx_hook(void *win_ptr, int x_event, int x_mask, int (*funct)(), void *param): event trigger depending on the function you use
+	//win_ptr: window to apply the hook
+	//x_event: event to trigger the hook
+	//x_mask: mask to trigger the hook
+	//funct: function to trigger when the event is triggered
+	//param: parameter to pass to the function
+	mlx_hook(data.win, DestroyNotify, NoEventMask, close_win, NULL); 	//for close window with X button
+	mlx_hook(data.win, KeyPress, KeyPressMask, button, NULL);	//for close window with ESC button
+	mlx_hook(data.win, MotionNotify, PointerMotionMask, handle_mouse, &data);	//for mouse move
 
 	//put image to window
 	mlx_put_image_to_window(data.mlx, data.win, data.img, 0, 0);
