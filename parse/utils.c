443016083a3bf5322bd855526e49540ff47f648c
Cheng Soo Tan <chtan@u82z01s02.42kl.edu.my>
2024-12-31 12:22:43 +0800

    fix: norm error

--- parse/utils.c
+++ parse/utils.c
@@ -6,70 +6,70 @@
 /*   By: chtan <chtan@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 15:21:52 by chtan             #+#    #+#             */
-/*   Updated: 2024/12/31 11:32:02 by chtan            ###   ########.fr       */
+/*   Updated: 2024/12/31 12:21:51 by chtan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
-void    ft_error(char *str)
+void	ft_error(char *str)
 {
-    printf("\033[0;32mError : %s\033[0m\n", str);
+	printf ("\033[0;32mError : %s\033[0m\n", str);
 }
 
-size_t    ft_len(char *s)
+size_t	ft_len(char *s)
 {
-    size_t    i;
+	size_t	i;
 
-    i = 0;
-    while (s[i])
-        i++;
-    return (i);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-char    *remove_nl(char *src)
+char	*remove_nl(char *src)
 {
-    int    len;
+	int	len;
 
-    len = ft_strlen(src);
-    if (src[len - 1] == '\n')
-        src[len - 1] = '\0';
-    return (src);
+	len = ft_strlen(src);
+	if (src[len - 1] == '\n')
+		src[len - 1] = '\0';
+	return (src);
 }
 
-char    **copy_2d_array(char **src, int start, int src_height)
+char	**copy_2d_array(char **src, int start, int src_height)
 {
-    int        j;
-    char    **dest;
+	int		j;
+	char	*dest;
 
-    j = 0;
-    dest = malloc(sizeof(char *) * (src_height - start + 1));
-    while (start < src_height)
-    {
-        dest[j] = ft_strdup(remove_nl(src[start]));
-        start++;
-        j++;
-    }
-    dest[j] = NULL;
-    return (dest);
+	j = 0;
+	dest = malloc(sizeof(char *) * (src_height - start + 1));
+	while (start < src_height)
+	{
+		dest[j] = ft_strdup(remove_nl(src[start]));
+		start++;
+		j++;
+	}
+	dest[j] = NULL;
+	return (dest);
 }
 
-int    ft_strspn(const char *s, const char *accept)
+int	ft_strspn(const char *s, const char *accept)
 {
-    int    count;
-    int    i;
+	int	count;
+	int	i;
 
-    count = 0;
-    i = 0;
-    while (*s)
-    {
-        while (accept[i] && *s != accept[i])
-            i++;
-        if (accept[i] == '\0')
-            return (count);
-        i = 0;
-        count++;
-        s++;
-    }
-    return (count);
+	count = 0;
+	i = 0;
+	while (*s)
+	{
+		while (accept[i] && *s != accept[i])
+			i++;
+		if (accept[i] == '\0')
+			return (count);
+		i = 0;
+		count++;
+		s++;
+	}
+	return (count);
 }
